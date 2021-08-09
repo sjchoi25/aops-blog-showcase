90f64ff01d456480250c81ea899b6ff319eba591
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-08-09 13:01:43 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,10 +54,3 @@ const nav_default = `
 <a href="#" id="blog-title" onclick="hide_comments()">${title ? title : "My Blog!"}</a>`;
 
 let replaced = false;
-
-$('#wrapper').prepend(`
-<div id="header">
-    <a data-blog="" href="#">
-    	<h1>${title ? title : "My Blog!"}</h1>
-    </a>
-</div>`)
